1c853c98d6b0c7d94486f2108783941e04c7e5cb
Abolaji Berachaiah <berachaiah.abolaji@gmail.com>
2026-05-09 19:19:33 +0100

    Fix duplicate URL namespace warning

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('task_a.urls')),
-    path('api/task-a/', include('task_a.urls')),
+    path('', include('task_a.urls', namespace='task_a')),
+    path('api/task-a/', include('task_a.urls', namespace='task_a_api')),
     path('api/task-b/', include('task_b.urls')),
 ]
